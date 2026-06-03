4d0c0a3ee5b8dd1df8ee7d92df0bea1b379b0958
edoardosarri24 <sarriedoardo24@gmail.com>
2026-06-04 00:09:52 +0200

    safety monitor bug fix

--- src/inference/include/safety_monitor.hpp
+++ src/inference/include/safety_monitor.hpp
@@ -6,7 +6,9 @@
 
 namespace safety_monitor {
 
-    constexpr double safe_acceleration();
+    inline constexpr double safety_monitor::safe_acceleration() {
+        return physic_parameters::MIN_ACCELLERATION;
+    }
     double cirtical_distance(
         double const leader_velocity,
         double const ego_velocity);
--- src/inference/src/safety_monitor.cpp
+++ src/inference/src/safety_monitor.cpp
@@ -2,16 +2,12 @@
 #include "global_parameters.hpp"
 #include <cmath>
 
-constexpr double safety_monitor::safe_acceleration() {
-    return physic_parameters::MIN_ACCELLERATION;
-}
-
 double safety_monitor::cirtical_distance(double const leader_velocity, double const ego_velocity) {
         return ego_velocity * simulation_parameter::dt
             + (
                 (std::pow(ego_velocity,2) - std::pow(leader_velocity,2))
                 /
-                (2 * physic_parameters::MIN_ACCELLERATION)
+                (2 * std::abs(physic_parameters::MIN_ACCELLERATION))
             );
     }
 
